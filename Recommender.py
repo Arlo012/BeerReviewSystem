c76b370f6a0a2085c56ede2091d378d014124285
Cameron <camdubyahill@gmail.com>
2015-05-09 13:49:24 -0400

    Added a searching function when rating beers.

--- Recommender.py
+++ Recommender.py
@@ -22,7 +22,7 @@ def Recommender(beerReviews):  #argument is a map of beer names to a list that i
 	beerMap = pickle.load(beerMapFile)
 	userBeerReviewMatrix = pickle.load(matrixFile)
 
-	userBeerArray = csr_matrix((1,len(beerMap)), dtype = int)
+	userBeerArray = csr_matrix((1,len(beerMap)), dtype = int)  #your rating of all the beers
 
 	iterator = iter(beerReviews)
 	for iterator in beerReviews:
@@ -33,85 +33,90 @@ def Recommender(beerReviews):  #argument is a map of beer names to a list that i
 
 
 
-	similarityArray = []
-	magnitude1 = math.sqrt(userBeerArray.multiply(userBeerArray).sum())
-	userBeerArray = userBeerArray.transpose()
+	similarityArray = []		#an array of your similarity to each other user in the correct indexing order
+	magnitude1 = math.sqrt(userBeerArray.multiply(userBeerArray).sum())  #Your Magnitude
+	userBeerArray = userBeerArray.transpose()		#Transpose so that this array can be dot producted
 	print magnitude1
 
 	# print userBeerReviewMatrix.get_shape()[0]
 	# nextuser = userBeerReviewMatrix.getrow(6543)
 	# print nextuser
 
-	for i in range(0, userBeerReviewMatrix.get_shape()[0]):
-		nextUser = userBeerReviewMatrix.getrow(i)
-		dot = nextUser*userBeerArray
-		magnitude2 = math.sqrt(nextUser.multiply(nextUser).sum())
-		similarity = dot[0,0]/magnitude1/magnitude2
-		similarityArray.append(similarity)
+	for i in range(0, userBeerReviewMatrix.get_shape()[0]):  #iterate through every user
+		nextUser = userBeerReviewMatrix.getrow(i)			#each row represents a user
+		dot = nextUser*userBeerArray						#dot you with every other user
+		magnitude2 = math.sqrt(nextUser.multiply(nextUser).sum())		#getting the user's magnitude
+		similarity = dot[0,0]/magnitude1/magnitude2			#Finding the cosine similarity with this user
+		similarityArray.append(similarity)					#add it to the list
 
-	userBeerArray = userBeerArray.transpose()
-	top = []
-	topIndex = []
+	userBeerArray = userBeerArray.transpose()		#untranspose from before
+	top = []				#This array will contain the similarity of the top ten people
+	topIndex = []			#this array of the top indexes
 
-	for i in range(0, len(similarityArray)):
-		if i < numberOfTopReviewers:
-			top.append(similarityArray[i])
+	for i in range(0, len(similarityArray)):			#cycle through all the users
+		if i < numberOfTopReviewers:					#for the first 10 or so people
+			top.append(similarityArray[i])				
 			topIndex.append(i)
 		else:
-			top.sort()
-			if similarityArray[i] > top[0]:
-				top[0] = similarityArray[i]
-				topIndex.append(i)
+			top.sort()									#sort the top similarity
+			if similarityArray[i] > top[0]:				#if my next value is greater than the smallest value in the list
+				top[0] = similarityArray[i]				#replace the smallest value with the greater value
+				topIndex.append(i)						#add this to the list of top indexes
 
 	print top
 	topIndex.reverse()
-	topIndex = topIndex[0:numberOfTopReviewers]
+	topIndex = topIndex[0:numberOfTopReviewers]			#finds the top ten indexes for people
 	topIndex.sort()
 	print topIndex
 
-	topReviewRows = []
+	topReviewRows = []				#stores a list of sparse arrays that contain the top ten most similar users beer reviews
 
-	for i in range(0, numberOfTopReviewers):
+	for i in range(0, numberOfTopReviewers):				#get the rows that I need
 		topReviewRows.append(userBeerReviewMatrix.getrow(topIndex[i]))
 
 	columnAverages = []
 	topBeers = []
 	topBeersIndex = []
 
-	for i in range(0, userBeerReviewMatrix.get_shape()[1]):
+	for i in range(0, userBeerReviewMatrix.get_shape()[1]):			#A loop to calculate the average rating of the beers
 		nextSum = 0
+		nonZeroReviewCount = 0
 		for j in range(0, numberOfTopReviewers):
-			nextSum = nextSum + topReviewRows[j][0,i]
-
-		nextAverage = float(nextSum)/numberOfTopReviewers
+			if topReviewRows[j][0,i] != 0:
+				nextSum = nextSum + topReviewRows[j][0,i]
+				nonZeroReviewCount += 1
+		if nonZeroReviewCount != 0:
+			nextAverage = float(nextSum)/nonZeroReviewCount		#average review of the current Beer
+		else:
+			nextAverage = 0		
 		if nextAverage != 0:
 			print nextAverage
 		columnAverages.append(nextAverage)
 
-		if i < numberOfTopBeers:
-			topBeers.append(nextAverage)
+		if i < numberOfTopBeers:							#finds the top ten Beers
+			topBeers.append(nextAverage)					#for the first couple people...
 			topBeersIndex.append(i)
-		else:
+		else:												#for the rest...same algorithm as with finding the top 10 people
 			topBeers.sort()
-			if (nextAverage > topBeers[0]) and (userBeerArray[0,i] == 0): 
+			if (nextAverage > topBeers[0]) and (userBeerArray[0,i] == 0):   #also checks if beer is already in your list of reviewed beers
 				topBeers[0] = nextAverage
 				topBeersIndex.append(i)
 
-	topBeersIndex.reverse()
+	topBeersIndex.reverse()							#same as above
 	topBeersIndex = topBeersIndex[0:numberOfTopBeers]
 	topBeersIndex.sort()
 
 	print topBeers
 	print topBeersIndex
 
-	recommendationList = []
+	recommendationList = []			#returns the list of the top recommendations
 
 	iterator = iter(beerMap)
 	for iterator in beerMap:
 		currentBeer = beerMap.get(iterator)
 		if currentBeer.matrixIndex in topBeersIndex:
 			print currentBeer.name
-			recommendationList.append(currentBeer.name)
+			recommendationList.append(currentBeer.name)				#finds the name rather than the index of the beer
 
 
 	return recommendationList
